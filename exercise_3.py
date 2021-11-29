798d52c3b1806b921eccaf4ad39034b0802d0065
poooooooooope <digitsou0l@gmail.com>
2021-11-30 03:23:57 +0600

    exercises done

--- exercise_3.py
+++ exercise_3.py
@@ -1,6 +1,43 @@
-list = ["Element", "start", "finish"]
+# 1
 
-def func(a):
-    list.append(a)
+def func(llll):
+    llll.insert(0, "Element")
+    llll.insert(1, "start")
+    llll.append("finish")
+    return llll
 
-func(['hello', 5, 'John', ])
+print(func(['hello', 5, 'John', ]))
+
+# 2
+
+def func_1(*args):
+    arg1, arg2, arg3 = args
+    a = {}
+    a[arg1] = 1
+    a[arg2] = 2
+    a[arg3] = 3
+    return a
+
+print(func_1('x', 5, 'John'))
+
+# 3
+
+def func_3(a):
+    x = list(a)
+    y = []
+    z = []
+
+    for element in x:
+        c = element % 2 == 0
+        if c:
+            y.append(element)
+    for lesr in x:
+        d = lesr ** 2
+        z.append(d)
+
+    print(y)
+    print(z)
+
+func_3((1,2,3,4,5))
+
+
