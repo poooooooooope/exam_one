612f5017fa0087cfbc5d6a896048bbf4381b5aeb
poooooooooope <digitsou0l@gmail.com>
2021-11-29 19:48:23 +0600

    exircise1.py done

---
+++ exercise_1.py
@@ -0,0 +1,19 @@
+# 1 x = str(int(float(5))), x - str
+
+# 2 x = 'aa' == 'AA', x - str
+
+# 3 x = {i: i**2 for i in range(5)}, x - dict
+
+# 4 x = set(list((5, 6, 7))) - set
+
+# 5 a = {1: 1, 2: 4, 3: 9} x = a.get(4) - int
+
+# 6 x = [].append('j') - list
+
+# 7 for i in range(10): if i < 5: x = 'hello' else: x = 5, x - int
+
+# 8 x = input('Enter and integer: '), x - str
+
+# 9 a = 5 b = [1, 3, 5, ] x = 'x' a, b, x = x, a, b, x - list
+
+# 10 def func(x, y=5): return x + y x = func('Jaguar ', 'hunter'), x - str
